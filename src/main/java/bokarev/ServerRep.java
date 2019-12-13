db7df50e06b9484b755256c4f8fa0e9c30d3c19b
BokarevDmitry <dbokarevv@gmail.com>
2019-12-13 22:24:47 +0300

    Scripted auto-commit on change (2019-12-13 22:24:47) by gitwatch.sh

--- src/main/java/bokarev/ServerRep.java
+++ src/main/java/bokarev/ServerRep.java
@@ -1,4 +1,23 @@
 package bokarev;
 
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
 public class ServerRep {
+    public static void main(String[] args) {
+        ZContext context = new ZContext();
+        ZMQ.Socket socket = context.createSocket(SocketType.REP);
+        try {
+            socket.bind("tcp://localhost:5555");
+            System.out.println("bind");
+            while (!Thread.currentThread().isInterrupted()) {
+                String req = socket.recvStr();
+                socket.send("reply!" + req);
+            }
+        } finally {
+            context.destroySocket(socket);
+            context.destroy();
+        }
+    }
 }
