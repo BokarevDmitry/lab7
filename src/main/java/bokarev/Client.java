550bd62f38c075e701b40f7e90545e6e4676f2f6
BokarevDmitry <dbokarevv@gmail.com>
2019-12-14 22:08:32 +0300

    Scripted auto-commit on change (2019-12-14 22:08:32) by gitwatch.sh

--- src/main/java/bokarev/Client.java
+++ src/main/java/bokarev/Client.java
@@ -1,4 +1,37 @@
 package bokarev;
 
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ.Socket;
+import org.zeromq.ZMsg;
+
+import java.util.Scanner;
+
+
 public class Client {
+
+    private static final String FRONTEND_ADDR = "tсp://localhost:5559";
+
+    public static void main(String[] args) {
+        try (ZContext context = new ZContext()){
+            Socket req = context.createSocket(SocketType.REQ);
+            req.connect(FRONTEND_ADDR);
+            Scanner in = new Scanner(System.in);
+            while (true) {
+                String[] command = in.nextLine().split(" ");
+                if (command[0].equals("STOP")) {
+                    break;
+                }
+                ZMsg message = new ZMsg();
+                for (int i=0; i<command.length; i++) {
+                    message.add(command[i]);
+                }
+                message.send(req);
+                ZMsg response = ZMsg.recvMsg(req);
+                System.out.println(response.pop().toString());
+                response.destroy();
+                message.destroy();
+            }
+        }
+    }
 }
